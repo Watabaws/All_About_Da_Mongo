7ebe411becde7d1987be573ed4c5ee57894f0ac7
Adam Abbas <aabbas@stuy.edu>
2018-02-14 13:19:16 -0500

    Class work!

---
+++ code.py
@@ -0,0 +1,19 @@
+c = MongoClient('lisa.stuy.edu', 27017)
+DB = c.test
+restaraunt = mfDB['restaurants']
+
+
+def ret_boroughs(borough):
+    return restaraunt.find({"borough": borough})
+
+def ret_zip(zip):
+    return restaraunt.find({"zip": zip})
+
+def ret_zipGrade(zip, grade):
+    zips = ret_zip(zip)
+    zip_grades = zips.find({"zip": zip}, {"grade" : grade});
+    return zip_grades
+
+def ret_zipMaxgrade(zip, grade):
+    zips = ret_zip(zip)
+    zip_grades = zips.find({"zip": zip}, {"grade": $lt})
